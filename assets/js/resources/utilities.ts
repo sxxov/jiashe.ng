8e853d2cf3963eca8fe9c6c260d83d1b719da60d
sxxov <sxxovnetwork@gmail.com>
2020-04-02 19:19:03 +0800

    Implement 'PromiseQueueUtility'

--- assets/js/resources/utilities.ts
+++ assets/js/resources/utilities.ts
@@ -2,3 +2,4 @@ export * from './utilities/$.utility.js';
 export * from './utilities/for.utility.js';
 export * from './utilities/window.utility.js';
 export * from './utilities/bezier.utility.js';
+export * from './utilities/promiseQueue.utility.js';
