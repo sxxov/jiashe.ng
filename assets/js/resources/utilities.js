974dd4a80316b19ed7af3536e00a82fa49f43884
sxxov <sxxovnetwork@gmail.com>
2020-04-02 19:19:03 +0800

    Implement 'PromiseQueueUtility'

--- assets/js/resources/utilities.js
+++ assets/js/resources/utilities.js
@@ -2,4 +2,5 @@ export * from './utilities/$.utility.js';
 export * from './utilities/for.utility.js';
 export * from './utilities/window.utility.js';
 export * from './utilities/bezier.utility.js';
+export * from './utilities/promiseQueue.utility.js';
 //# sourceMappingURL=utilities.js.map
