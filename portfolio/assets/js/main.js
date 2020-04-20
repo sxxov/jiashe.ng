d1c64806986bebacce6d82e0ccb25dd4671f4888
sxxov <sxxovnetwork@gmail.com>
2020-04-20 21:44:24 +0800

    Start implementation of horizontal scroll mapping

--- portfolio/assets/js/main.js
+++ portfolio/assets/js/main.js
@@ -7,11 +7,13 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-import { $ } from '../../../assets/js/resources/utilities.js';
+import { $, BezierUtility } from '../../../assets/js/resources/utilities.js';
 const { marked } = window;
 class Main {
     constructor() {
         this.skinDom = $('.skin');
+        this.scrollRafId = null;
+        this.mBezierUtility = new BezierUtility(0.075, 0.82, 0.165, 1);
     }
     create() {
         return __awaiter(this, void 0, void 0, function* () {
@@ -24,6 +26,48 @@ class Main {
         uri += uri.substr(-3) === '.md' ? '' : '.md';
         uri = `/assets/md/${uri}`;
         return uri;
+    }
+    onVerticalScroll(event) {
+        if (!event.deltaY) {
+            return;
+        }
+        const delta = event.deltaY + event.deltaX;
+        cancelAnimationFrame(this.scrollRafId);
+        const currentTarget = $(event.currentTarget);
+        currentTarget.scrollLeft = this.expectedScrollLeft;
+        if (currentTarget.scrollLeft !== this.expectedScrollLeft) {
+            this.expectedScrollLeft = Number(currentTarget.scrollLeft);
+            return;
+        }
+        this.cachedScrollLeft = this.expectedScrollLeft;
+        this.expectedScrollLeft += delta;
+        let i = 0;
+        const handler = () => {
+            const magic = this.mBezierUtility.getValue((Math.abs(i))
+                / Math.abs(delta)) * delta;
+            switch (true) {
+                case delta < 0: {
+                    if (i < delta) {
+                        return;
+                    }
+                    i -= 2;
+                    break;
+                }
+                case delta > 0: {
+                    if (i > delta) {
+                        return;
+                    }
+                    i += 2;
+                    break;
+                }
+                default: {
+                    return;
+                }
+            }
+            currentTarget.scrollLeft = magic + this.cachedScrollLeft;
+            this.scrollRafId = requestAnimationFrame(() => handler());
+        };
+        this.scrollRafId = requestAnimationFrame(() => handler());
     }
 }
 (() => __awaiter(void 0, void 0, void 0, function* () {
--- portfolio/assets/js/main.ts
+++ portfolio/assets/js/main.ts
@@ -1,10 +1,16 @@
 import { MarkedNamespace } from './raw/libraries/marked/types/marked.js';
-import { $ } from '../../../assets/js/resources/utilities.js';
+import { $, BezierUtility } from '../../../assets/js/resources/utilities.js';
 
 const { marked }: { marked: MarkedNamespace['marked'] } = window as any;
 
 class Main {
 	private skinDom = $('.skin');
+
+	private expectedScrollLeft: number;
+	private cachedScrollLeft: number;
+	private scrollRafId: number = null;
+
+	private mBezierUtility = new BezierUtility(0.075, 0.82, 0.165, 1);
 
 	public async create(): Promise<void> {
 		this.skinDom.innerHTML = marked(await (await fetch(this.uri)).text());
@@ -18,6 +24,67 @@ class Main {
 		uri = `/assets/md/${uri}`;
 
 		return uri;
+	}
+
+	public onVerticalScroll(event: MouseWheelEvent): void {
+		if (!event.deltaY) {
+			return;
+		}
+
+		const delta = event.deltaY + event.deltaX;
+
+		cancelAnimationFrame(this.scrollRafId);
+
+		const currentTarget = $(event.currentTarget);
+
+		currentTarget.scrollLeft = this.expectedScrollLeft;
+
+		if (currentTarget.scrollLeft !== this.expectedScrollLeft) {
+			this.expectedScrollLeft = Number(currentTarget.scrollLeft);
+
+			return;
+		}
+
+		this.cachedScrollLeft = this.expectedScrollLeft;
+		this.expectedScrollLeft += delta;
+
+		let i = 0;
+		const handler = (): void => {
+			const magic = this.mBezierUtility.getValue(
+				(
+					Math.abs(i)
+				)
+				/ Math.abs(delta),
+			) * delta;
+
+
+			switch (true) {
+			case delta < 0: {
+				if (i < delta) {
+					return;
+				}
+
+				i -= 2;
+				break;
+			}
+			case delta > 0: {
+				if (i > delta) {
+					return;
+				}
+
+				i += 2;
+				break;
+			}
+			default: {
+				return;
+			}
+			}
+
+			currentTarget.scrollLeft = magic + this.cachedScrollLeft;
+
+			this.scrollRafId = requestAnimationFrame(() => handler());
+		};
+		this.scrollRafId = requestAnimationFrame(() => handler());
 	}
 }
 
